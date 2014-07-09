5c68bd1c61911ee1232d6f2d88dfde78a4676c5b
Berjan Bruens <berjan@bruens.it>
2014-07-09 18:16:22 +0200

    -adding users to the group dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,16 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.db.models.signals import post_save
 
 __author__ = 'Bair1'
 
 
 class CustomUser(AbstractUser):
     keyboard_shortcuts = models.BooleanField(default=True)
+
+def add_user_to_dashboard_group(sender, instance, created, **kwargs):
+    dashboard_group, _ = Group.objects.get_or_create(name='dashboard')
+    if created:
+        instance.groups.add(dashboard_group)
+post_save.connect(add_user_to_dashboard_group, sender=CustomUser)
 
